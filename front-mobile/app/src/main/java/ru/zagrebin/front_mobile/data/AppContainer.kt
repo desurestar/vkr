9d36cc68c632d47279f5053054be99f65fe54561
klow <klowslow52@gmail.com>
2026-05-29 23:03:07 +0300

    commit

--- front-mobile/app/src/main/java/ru/zagrebin/front_mobile/data/AppContainer.kt
+++ front-mobile/app/src/main/java/ru/zagrebin/front_mobile/data/AppContainer.kt
@@ -71,7 +71,8 @@ class AppContainer(context: Context) {
     val refreshArticleDetailsUseCase = RefreshArticleDetailsUseCase(repository)
 
     private companion object {
-        const val BASE_URL = "http://192.168.4.103:8080/"
+        // const val BASE_URL = "http://192.168.4.103:8080/"
+        const val BASE_URL = "http://192.168.0.4:8080/"
         // const val BASE_URL = "http://10.0.2.2:8080/"
 
         private val lock = Any()
