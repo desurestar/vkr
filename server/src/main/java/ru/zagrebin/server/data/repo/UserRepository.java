5208c710f83396acb272b19ccc68ebdaf76a11d8
Dmitry Zagrebin <95430157+desurestar@users.noreply.github.com>
2026-06-02 20:18:59 +0300

    fix server (public profile)

--- server/src/main/java/ru/zagrebin/server/data/repo/UserRepository.java
+++ server/src/main/java/ru/zagrebin/server/data/repo/UserRepository.java
@@ -1,6 +1,8 @@
 package ru.zagrebin.server.data.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.zagrebin.server.data.entity.UserEntity;
 
 import java.util.Optional;
@@ -10,4 +12,12 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsernameIgnoreCase(String username);
     boolean existsByEmailIgnoreCase(String email);
     boolean existsByUsernameIgnoreCase(String username);
+
+    @Query("""
+            select count(followed) > 0
+            from UserEntity follower
+            join follower.following followed
+            where follower.id = :viewerId and followed.id = :userId
+            """)
+    boolean isFollowing(@Param("viewerId") Long viewerId, @Param("userId") Long userId);
 }
